1aa67fd75e084dd40b83e533a329fd7e9dd819af
minha <minhajulmalay@gmail.com>
2024-08-14 12:15:36 +0800

    boss

---
+++ 5lab2.py
@@ -0,0 +1,3 @@
+temp = int(input("Enter Fahrenheit temperature: "))
+#cel = float((5.0/9.0)*(temp-32))
+print(f")={temp} Fahrenheit is equals to {(float((5.0/9.0)*(temp-32))):.3f} Celsius\nHave a nice day. Thank you.")
